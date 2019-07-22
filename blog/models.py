90589a3bff5ff68429b9a632a76fbbe66466df9c
mufengcoding <mufeng5619@gamil.com>
2019-07-22 17:47:26 +0800

    编写新增文章

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
         (STATUS_DELETE, '删除')
     )
 
-    name = models.CharField(max_length=50, verbose_name="姓名")
+    name = models.CharField(max_length=50, verbose_name="名称")
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     is_nav = models.BooleanField(default=False, verbose_name="是否导航")
     owner = models.ForeignKey(User, verbose_name="作者",on_delete=models.CASCADE)
@@ -20,6 +20,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name = verbose_name_plural = '分类'
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
